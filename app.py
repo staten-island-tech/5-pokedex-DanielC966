2b4ad217f8f9b5591ccd3b27c89dc385298e006c
DanielC966 <danielc966@nycstudents.net>
2026-02-13 09:18:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,25 @@ data = json.load(pokedex)
 print(data[0])
 
 # Create a function that will take the data from the JSON file and you will iterate through the list of pokemon and print each pokemons name.
+def printNames(lang):
+    print("Names: ")
+    for i in range(len(data)):
+        print(" ", data[i]["name"][lang])
 
 # Add a language choice feature and print the pokemons name based on the user input
+
+print("Languages: ")
+print(" - English")
+print(" - Japanese")
+print(" - Chinese")
+print(" - French")
+language = input("Choose a language: ")
+while language.lower() != "english" or "japanese" or "chinese" or "french":
+    print("Language not found") 
+    language = input("Choose a language: ")
+
+
+printNames(language.lower())
 
 # Develop a function that creates a new list of pokemon based on the type the user searched for. If no pokemon was found of that type inform the user
 
